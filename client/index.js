c62c037121a23f461f191dd34379a6f3febc615a
paulsimroth <paul.simroth@hotmail.com>
2022-09-19 19:06:20 +0200

    ethers implementation in trial

--- client/index.js
+++ client/index.js
@@ -1,25 +1,26 @@
-import { ethers } from "ethers";
+let ethers = require('ethers');
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-await provider.send("eth_requestAccounts", []);
+
 const signer = provider.getSigner();
 
-const address = "dai.tokens.ethers.eth";
-const contract = new ethers.Contract(address, abi, provider);
+const contract = new ethers.Contract(contractAddress, abi, provider);
 
 let instance;
-let user;
-let contractAddress = "0x00c3fb12F09a1f00bbbAf512DDad3F3c612F565C";
+let user = await ethereum.request({ method: 'eth_requestAccounts' });;
+let contractAddress = "0x53DB98E520DDcDf6A04bF5eC8Ca2FfCa8a579956";
 
 $(document).ready(function(){
     window.ethereum.enable().then(function(accounts){
         instance = new ethers.Contract(contractAddress, abi, provider);
+        contract.attach(address)
         user = accounts[0];
 
         console.log(instance);
 
-        //Birth Event
-        contract.Birth().on("data", function (event) {
+        //Birth Event 
+        /*
+        contract.filters.Birth().on("data", function (event) {
             console.log(event);
             let owner = event.returnValues.owner;
             let bearId = event.returnValues.bearId;
@@ -38,12 +39,24 @@ $(document).ready(function(){
             $("#bearCreation").css("display", "block"),
             $("#bearCreation").css("background-color", "#ff471a"),
             $("#bearCreation").text("ERROR: Birth Event malfunctioned"));
+*/
+        //test for ethers
+        contract.on("Birth", (owner, bearId, mumId, dadId, genes) => {
+            console.log(`birth event ${owner} | ${bearId} | ${mumId} | ${dadId} | ${genes} `);
+
+            $("#bearCreation").css("display", "block");
+            $("#bearCreation").text("owner: " + owner
+                                    + "bearId: " + bearId
+                                    + "mumId: " + mumId
+                                    + "dadId: " + dadId
+                                    + "genes: " + genes)
+        });
     });
 })
 
 function createBear(){
     let dnaString = getDna();
-    contract.createBearGen0(dnaString).send({}, function(error, txHash){
+    contract.methods.createBearGen0(dnaString).send({}, function(error, txHash){
         if(error){
             alert("Create Bear, ERROR");
             console.log(error);
